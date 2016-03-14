ee5ecc345f8ecf2257ce7d702b50bbae02385d39
Madan, Smarth <smadan@paypal.com>
2016-03-14 14:18:48 -0700

    minor fix and unit test addition

--- test/test_downloadobject.py
+++ test/test_downloadobject.py
@@ -9,7 +9,8 @@ class TestDownloadObject(unittest.TestCase):
     name = "multiarch-support_2.19-18+deb8u3_amd64.deb"
     pkg_path = "packages"
     dep = [{"name":"glibc/multiarch-support_2.19-18+deb8u3_amd64.deb","repo":"security","url":"http://security.debian.org/debian-security/pool/updates/main/g/glibc/multiarch-support_2.19-18+deb8u3_amd64.deb"},
-           {"name": "mawk/mawk_1.3.3-17_amd64.deb","repo": "main", "url":"http://ftp.us.debian.org/debian/pool/main/m/mawk/mawk_1.3.3-17_amd64.deb"}]
+           {"name": "mawk/mawk_1.3.3-17_amd64.deb","repo": "main", "url":"http://ftp.us.debian.org/debian/pool/main/m/mawk/mawk_1.3.3-17_amd64.deb"},
+           {"name": "libs/libselinux/libselinux1_2.3-2_amd64.deb","repo": "main", "url":"http://ftp.us.debian.org/debian/pool/main/libs/libselinux/libselinux1_2.3-2_amd64.deb"}]
     for d in dep:
       do = build_uri(d,self.repoyml)
       self.assertEqual(do,d["url"])
